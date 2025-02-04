8df6f0140c6a4013f535b447b1eac5a1641f211e
Virginie16 <63580406+Virginie16@users.noreply.github.com>
2025-02-04 16:24:15 +0100

    first commit

---
+++ src/test/java/com/example/demoSQLServer/DemoSqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSQLServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
